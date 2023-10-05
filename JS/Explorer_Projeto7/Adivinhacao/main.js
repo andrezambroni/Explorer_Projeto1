716db88ab756f24e0a0a9ad1b426ead4690609d5
andrezambroni <andrezambroni@gmail.com>
2023-10-05 16:53:22 -0300

    att adv

--- JS/Explorer_Projeto7/Adivinhacao/main.js
+++ JS/Explorer_Projeto7/Adivinhacao/main.js
@@ -1,17 +1,34 @@
-const randomNumber = Math.round(Math.random() * 10)
-let nTentativas = 1
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+const randomNumber = Math.round(Math.random() * 10);
+let xAttempts = 1;
 
-function handleClick(event) {
-  event.preventDefault()
+// função callback
+function handleTryClick(event) {
+  event.preventDefault();
 
-  const inputNumber = document.querySelector('#inputNumber')
+  const inputNumber = document.querySelector("#inputNumber");
 
   if (Number(inputNumber.value) == randomNumber) {
-    document.querySelector('.screen1').classList.add('hide')
-    document.querySelector('.screen2').classList.remove('hide')
+    screen1.classList.add("hide");
+    screen2.classList.remove("hide");
 
-    document.querySelector('.screen2 h2').innerText = `acertou em ${nTentativas} tentativas`
+    document.querySelector(
+      ".screen2 h2"
+    ).innerText = `acertou em ${xAttempts} tentativas`;
   }
 
-  nTentativas++
+  inputNumber.value = "";
+  xAttempts++;
 }
+
+// Eventos
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
+
+btnTry.addEventListener("click", handleTryClick);
+btnReset.addEventListener("click", function () {
+  screen1.classList.remove("hide");
+  screen2.classList.add("hide");
+  xAttempts = 1;
+});
