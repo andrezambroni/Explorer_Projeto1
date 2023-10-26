97425df20142d3d9b31290edcf2748bde6698ce4
andrezambroni <andrezambroni@gmail.com>
2023-10-26 18:14:02 -0300

    att calc imc

--- JS/Explorer_Projeto7/CalculadoraIMC/script.js
+++ JS/Explorer_Projeto7/CalculadoraIMC/script.js
@@ -6,13 +6,27 @@ const modalWrapper = document.querySelector('.modal-wrapper')
 const modalMenssage = document.querySelector('.modal .tittle span')
 const modalBtnClose = document.querySelector('.modal button.close')
 
+modalMenssage.innerText = 'qualquer coisa'
+modalWrapper.classList.add('open')
+
 form.onsubmit = (event) => {
   event.preventDefault();
   const weight = inputWeight.value;
   const height = inputHeight.value;
 
   const result = IMC(weight, height);
-};
+  const message = `Seu IMC é de ${result}`
+
+  modalMenssage.innerText=message
+
+  modalWrapper.classList.add('open')
+
+  
+}
+
+modalBtnClose.onClick = () => {
+    modalWrapper.classList.remove('open')
+}
 
 function IMC(weight, height) {
   return (weight / (height / 100) ** 2).toFixed(2);
