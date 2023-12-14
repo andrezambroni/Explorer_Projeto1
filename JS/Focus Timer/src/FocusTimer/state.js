10dbcd2d235ce73b4df624dbbab52356d612970b
andrezambroni <andrezambroni@gmail.com>
2023-12-14 18:02:50 -0300

    commit final

--- JS/Focus Timer/src/FocusTimer/state.js
+++ JS/Focus Timer/src/FocusTimer/state.js
@@ -1,6 +1,7 @@
-export default{
+export default {
     minutes: 25,
     seconds: 0,
     isRunning: false,
     isMute: true,
+    countdownId: null
 }
--- JS/Focus Timer/src/toogle-mode.js
+++ JS/Focus Timer/src/toogle-mode.js
@@ -1,9 +1,9 @@
-let DarkMode = true;
+let darkMode = true
 const buttonToggle = document.getElementById('toggle-mode')
 
 buttonToggle.addEventListener('click', (event) => {
     document.documentElement.classList.toggle('light')
-    const mode = DarkMode ? 'light' : 'dark'
+    const mode = darkMode ? 'light' : 'dark'
     event.currentTarget.querySelector('span').textContent = `${mode} mode ativado!`
-    DarkMode = !DarkMode
+    darkMode = !darkMode
 })
