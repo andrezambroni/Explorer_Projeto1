91aa2e3ce2794d5cb3d57f530af61ce50e0963fc
andrezambroni <andrezambroni@gmail.com>
2024-01-18 16:28:42 -0300

    params e nodemon

--- Node/src/server.js
+++ Node/src/server.js
@@ -3,15 +3,20 @@ const express = require("express");
 const app = express();
 
 app.get("/message/:id/:user", (request, response) => {
-
-    const { id, user} = resquest.params;
+  const { id, user } = resquest.params;
 
   response.send(`
-        Id da mendagem: ${id}. 
+        Id da mensagem: ${id}. 
         Para usuário: ${user}.
    `);
 });
 
+app.get("/users", (resquest, response) => {
+  const { page, limit } = request.query;
+
+  response.send(`Página: ${page}. Mostrar: ${limit}`);
+});
+
 const PORT = 3000;
 
-app.listen(PORT, () => console.log(`Server is runnig on Port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
