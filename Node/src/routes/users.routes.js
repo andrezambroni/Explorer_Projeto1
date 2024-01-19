3b21bb00022f50f3bf26ab3c46f517601fefe429
andrezambroni <andrezambroni@gmail.com>
2024-01-19 19:03:31 -0300

    execoes

--- Node/src/routes/users.routes.js
+++ Node/src/routes/users.routes.js
@@ -4,19 +4,10 @@ const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router();
 
-// function myMiddleware(request, response, next) {
-//   console.log("passou pelo middleware");
-
-//   if (!request.body.isAdmin) {
-//     return response.json({ message: "usuario nao autorizado" });
-//   }
-
-//   next();
-// }
 
 const usersController = new UsersController();
 
-usersRoutes.post("/", myMiddleware, usersController.create);
+usersRoutes.post("/", usersController.create);
 
 // estou esportando esse arquivo pra quem quiser utilizar
 module.exports = usersRoutes;
--- Node/src/server.js
+++ Node/src/server.js
@@ -1,11 +1,28 @@
-const express = require('express')
+require("express-async-errors");
 
-const routes = require('./routes')
+const express = require("express");
 
-const app = express()
-app.use(express.json())
+const routes = require("./routes");
+const AppError = require("./utils/AppError");
 
-app.use(routes)
+const app = express();
+app.use(express.json());
 
-const PORT = 3000
-app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
+app.use(routes);
+
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+        status: "error",
+        message: error.message
+    })
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "internal server error",
+  })
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
