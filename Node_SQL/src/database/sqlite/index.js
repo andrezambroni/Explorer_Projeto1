bb28f68af4d641b344b3728eb08188e23db48657
andrezambroni <andrezambroni@gmail.com>
2024-01-26 16:17:24 -0300

    fix routes and bugs

--- Node_SQL/src/database/sqlite/index.js
+++ Node_SQL/src/database/sqlite/index.js
@@ -1,15 +1,14 @@
-const sqlite3 = require('sqlite3');
-const sqlite = require('sqlite');
+const sqlite3 = require('sqlite3')
+const sqlite = require('sqlite')
+const path = require('path')
 
-const path = require('path');
+async function sqliteConnection(){
+  const database = await sqlite.open({
+    filename: path.resolve(__dirname, "..", "database.db"),
+    driver: sqlite3.Database
+  })
 
-async function sqliteConnection (){
-    const database = await sqlite3.open({
-        filename: path.resolve(__dirname, "..", "database.db"),
-        driver: sqlite3.Database
-    });
+  return database
+} 
 
-    return database;
-}
-
-module.exports = sqliteConnection;
+module.exports = sqliteConnection
