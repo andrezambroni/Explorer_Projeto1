c4e5013f456e939c32f5dc0eedf9f184cb5bae25
andrezambroni <andrezambroni@gmail.com>
2024-01-29 17:55:43 -0300

    atualizacao de senha

--- Node_SQL/src/controllers/UsersController.js
+++ Node_SQL/src/controllers/UsersController.js
@@ -2,7 +2,7 @@ const AppError = require("../utils/AppError");
 
 const sqlite = require("../database/sqlite");
 const sqliteConnection = require("../database/sqlite");
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 
 class UsersController {
   async create(request, response) {
@@ -30,7 +30,7 @@ class UsersController {
   }
 
   async update(request, response) {
-    const { name, email } = request.body;
+    const { name, email, password, old_password } = request.body;
     const { id } = request.params;
 
     const database = await sqliteConnection();
@@ -51,8 +51,25 @@ class UsersController {
 
     user.name = name;
     user.email = email;
+
+    if (password && !old_password) {
+      throw new AppError(
+        "Informe a senha atual para mudar o cadastro de senha."
+      );
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if (!checkOldPassword) {
+        throw new AppError("Senha atual incorreta");
+      }
+
+      user.password = await hash(password, 8);
+    }
+
     await database.run(
-      `UPDATE users SET name = ?, email = ?, updated_at = ?, WHERE id = ?`,
+      `UPDATE users SET name = ?, email = ?, password = ?, updated_at = ?, WHERE id = ?`,
       [user.name, user.email, newDate(), id]
     );
 
